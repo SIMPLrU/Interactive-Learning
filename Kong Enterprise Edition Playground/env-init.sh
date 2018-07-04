8e2a4d365e4e2daa9aee16176b531d98b7123ce4
SIMPLrU <viet@simplru.com>
2018-07-04 15:48:39 +0300

    fixed the script

--- Kong Enterprise Edition Playground/env-init.sh
+++ Kong Enterprise Edition Playground/env-init.sh
@@ -7,15 +7,3 @@ sleep 10s; echo "Running Kong migrations..."
 sleep 5s; echo "Installing Kong E.E...."
 sleep 5s; echo "Starting Kong E.E...."
 echo: YOUR KONG ENVIRONMENT IS READY!
-
-
-
-
-read -p  "Initializing..." -t 1
-read -p  "Logging in BinTray Credentials..." -t 1
-read -p  "Pulling the latest Kong E.E Image..." -t 10
-read -p  "Tagging your image for easier management..." -t 2
-read -p  "Running Kong migrations..." -t 5
-read -p  "Installing Kong E.E...." -t 10
-
-
