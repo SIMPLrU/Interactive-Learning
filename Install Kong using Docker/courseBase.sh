5705f6c686b733a1066bd97dde837bc6130482fa
SIMPLrU <viet@simplru.com>
2018-07-04 16:04:33 +0300

    added HTTPie Install

--- Install Kong using Docker/courseBase.sh
+++ Install Kong using Docker/courseBase.sh
@@ -1,3 +1,4 @@
 docker pull postgres:9.6  
 docker pull kong:latest
+echo y | apt-get install httpie
 
--- Kong Enterprise Edition Playground/courseBase.sh
+++ Kong Enterprise Edition Playground/courseBase.sh
@@ -13,3 +13,4 @@ sleep 5s
 docker run --rm --link kong-ee-database:kong-ee-database -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=kong-ee-database" -e "KONG_CASSANDRA_CONTACT_POINTS=kong-ee-database" -e "KONG_LICENSE_DATA=$KONG_LICENSE_DATA" kong-ee kong migrations up
 sleep 2s
 docker run -d --name kong-ee --link kong-ee-database:kong-ee-database -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=kong-ee-database" -e "KONG_CASSANDRA_CONTACT_POINTS=kong-ee-database" -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" -e "KONG_PROXY_ERROR_LOG=/dev/stderr" -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" -e "KONG_VITALS=on" -e "KONG_ADMIN_LISTEN=0.0.0.0:8001, 0.0.0.0:8444 ssl" -e "KONG_PORTAL=on" -e "KONG_PORTAL_GUI_URI=localhost:8003" -e "KONG_LICENSE_DATA=$KONG_LICENSE_DATA" -p 8000:8000 -p 8443:8443 -p 8001:8001 -p 8444:8444 -p 8002:8002 -p 8445:8445 -p 8003:8003 -p 8004:8004 kong-ee
+echo y | apt-get install httpie
