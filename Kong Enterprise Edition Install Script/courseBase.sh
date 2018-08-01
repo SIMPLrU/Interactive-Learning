637ee85a7bd51ece6d7363c9be51aa580681405f
SIMPLrU <viet@simplru.com>
2018-08-01 16:59:35 +0300

    removed

--- Kong Enterprise Edition Install Script/courseBase.sh
+++ Kong Enterprise Edition Install Script/courseBase.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-`bash <(curl -Ls https://raw.githubusercontent.com/SIMPLrU/Learn-SIMPLr/gh-pages/community/00_kong_ee.onclick.sh?token=AaHSZN4T8H9WkGAxi-ILNI6lmoMDosMyks5bauXgwA%3D%3D)
-`{{execute}}
+
 
 # ====EXIT script====
   exit 2    # Misuse of shell builtins (according to Bash documentation)
--- Kong Enterprise Edition Playground/courseBase.sh
+++
@@ -1,2 +0,0 @@
-`bash <(curl -Ls https://raw.githubusercontent.com/SIMPLrU/Learn-SIMPLr/gh-pages/community/00_kong_ee.onclick.sh?token=AaHSZN4T8H9WkGAxi-ILNI6lmoMDosMyks5bauXgwA%3D%3D)
-`{{execute}}
